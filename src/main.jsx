20d2171d463bd062c3e25541f4a17a27e4eddf0a
preethvi <139876295+preethvi@users.noreply.github.com>
2023-10-18 20:01:38 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Conditional from './components/Conditional.jsx'
+import List from './components/List.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
